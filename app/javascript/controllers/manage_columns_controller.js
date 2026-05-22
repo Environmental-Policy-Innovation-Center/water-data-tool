7133525e60bb0ef422f0b4929c2cf86bde9b3524
Corey Sheesley <corey.sheesley@gmail.com>
2026-05-22 15:25:12 -0600

    feat/add scaffolding for Manage Columns feature on data table

---
+++ app/javascript/controllers/manage_columns_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["button", "dropdown"]
+
+  #outsideClick = (e) => {
+    if (!this.element.contains(e.target)) this.#close()
+  }
+
+  connect() {
+    document.addEventListener("click", this.#outsideClick)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.#outsideClick)
+  }
+
+  toggle() {
+    this.dropdownTarget.classList.contains("hidden") ? this.#open() : this.#close()
+  }
+
+  #open() {
+    const rect = this.buttonTarget.getBoundingClientRect()
+    const dropdown = this.dropdownTarget
+    dropdown.style.top = `${rect.bottom + 8}px`
+    dropdown.style.right = "8px" // overhangs container-table's right-4 edge by the same 8px gap
+    dropdown.classList.remove("hidden")
+    dropdown.classList.add("flex")
+    this.buttonTarget.setAttribute("aria-expanded", "true")
+  }
+
+  #close() {
+    this.dropdownTarget.classList.remove("flex")
+    this.dropdownTarget.classList.add("hidden")
+    this.buttonTarget.setAttribute("aria-expanded", "false")
+  }
+}
