3cb3fe4d18faab2bd2b758efd058d6e0df97dc54
Corey Sheesley <corey.sheesley@gmail.com>
2026-06-20 10:46:38 -0600

    fix/report close button, from-map detection, remove dead back action

--- app/javascript/controllers/report_controller.js
+++ app/javascript/controllers/report_controller.js
@@ -1,6 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Map overlay: print and close (X). Standalone report layout: print only (back-to-map is link_to "/").
 export default class extends Controller {
   static targets = ["overlay", "frame"]
 
