65a40814633c54baf96a898ac1e638a885037bfd
EmmaLiTsai <emmali.tsai@gmail.com>
2026-09-17 15:10:14 -0400

    typo fix

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,7 +1,7 @@
 module AnalyticsHelper
   # This helper hooks us up to the appropriate google analytics workstream
   def ga_measurement_id
-    if request.host.== "watertool.policyinnovation.info"
+    if request.host == "watertool.policyinnovation.info"
       ENV["GA_MEASUREMENT_ID_PROD"]
     elsif request.host == "water-data-tool-staging.policyinnovation.info"
       ENV["GA_MEASUREMENT_ID_STAGING"]
