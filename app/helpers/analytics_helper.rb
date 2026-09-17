6de1218b5cbca9ce76350bad9cf8cb43e1e740bc
EmmaLiTsai <emmali.tsai@gmail.com>
2026-09-17 14:58:29 -0400

    edited host names

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,9 +1,9 @@
 module AnalyticsHelper
   # This helper hooks us up to the appropriate google analytics workstream
   def ga_measurement_id
-    if request.host.== "https://watertool.policyinnovation.info/"
+    if request.host.== "watertool.policyinnovation.info"
       ENV["GA_MEASUREMENT_ID_PROD"]
-    elsif request.host == "https://water-data-tool-staging.policyinnovation.info/"
+    elsif request.host == "water-data-tool-staging.policyinnovation.info"
       ENV["GA_MEASUREMENT_ID_STAGING"]
     end
   end
